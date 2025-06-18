6199e6fc56085e884f3ec2376646a0ed01c9dcd4
ArmandoD <armandoppws@gmail.com>
2025-06-17 23:11:05 -0500

    reduce logic for computer choice generation to arrow functions

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,18 +1,24 @@
- function generateComputerChoice() {
-    let generateComputerVaule = Math.floor(Math.random() * (3 - 1 + 1)) + 1; // generates a an integer between (min, max) -> inclusive 
-    if (generateComputerVaule == 1){
-        return "Rock"
-    }
-    if (generateComputerVaule == 2){
-        return "Paper"  
-    }
-    if (generateComputerVaule == 3){
-        return "Scissors"
-    }
+//  function generateComputerChoice() {
+//     let generateComputerVaule = Math.floor(Math.random() * (3 - 1 + 1)) + 1; // generates a an integer between (min, max) -> inclusive 
+//     if (generateComputerVaule == 1){
+//         return "Rock"
+//     }
+//     if (generateComputerVaule == 2){
+//         return "Paper"  
+//     }
+//     if (generateComputerVaule == 3){
+//         return "Scissors"
+//     }
+//  }
+
+
+ const computerValue = () => {
+    let value = Math.floor(Math.random() * 3) + 1;  // 1…3 inclusive
+    if (value == 1) return "Rock";
+    if (value == 2) return "Paper";
+    return "Scissors";
  }
 
-
-
  
- let computerChoice = generateComputerChoice();
+ let computerChoice = computerValue();
  console.log(computerChoice)
